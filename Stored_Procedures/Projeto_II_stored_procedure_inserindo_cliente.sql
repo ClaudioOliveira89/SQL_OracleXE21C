157f8ec56f3072afe53d8894364b924a65161eb5
Claudio Henrique de Oliveira Soares <87618859+ClaudioOliveira89@users.noreply.github.com>
2022-12-11 20:48:34 -0300

    Update Projeto_II_stored_procedure_inserindo_cliente.sql

--- Stored_Procedures/Projeto_II_stored_procedure_inserindo_cliente.sql
+++ Stored_Procedures/Projeto_II_stored_procedure_inserindo_cliente.sql
@@ -63,10 +63,10 @@ Select * from cliente;
 1	     Hugo Batista		    05/02/90	  51898608253
 2	     JosÃ© Antonio		  02/05/85	  51144722241
 3	     JoÃ£o Carlos		    03/05/93	  84882273292
-4	     Edivaldo Santana	        01/06/00	  30609426176
-5	     Cristina Oliveira	      16/08/99	  56313586700
+4	     Edivaldo Santana	              01/06/00	  30609426176
+5	     Cristina Oliveira	            16/08/99	  56313586700
 6	     Artur da Silva		  06/05/01	  67491246583
-7	     Adrina Domingues	      01/02/72	  22044617250
+7	     Adrina Domingues	              01/02/72	  22044617250
 8	     Amanda Silva		    02/02/88	  34778798201
 9	     Denis Soares		    14/10/72	  35423788102
 10	     Pedro Algusto		  21/10/82	  35477141002
