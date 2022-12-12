3d8aaac399d01d315e9180ece01cb9727dfb2134
Claudio Henrique de Oliveira Soares <87618859+ClaudioOliveira89@users.noreply.github.com>
2022-12-11 21:05:53 -0300

    Update Projeto_II_stored_procedure_inserindo_produto.sql

--- Stored_Procedures/Projeto_II_stored_procedure_inserindo_produto.sql
+++ Stored_Procedures/Projeto_II_stored_procedure_inserindo_produto.sql
@@ -53,13 +53,13 @@ SELECT * FROM produto;
 ID            Descrição                            Quantidade
 1	  SAMSUNG J7 - PRIME	               10
 2	  SAMSUNG J5 - PRIME	               10
-3	  IPHONE X		                      10
-4	  MOTO G 5S		                      10
-5	  IPHONE 6S		                             10
-9	  ONEPLUS 10PRO		                         12
-6	  SAMSUNG GALAXY S22	                     15
-7	  APPLE IPHONE 13 MINI	                   20
-8	  ASUS ZENFONE8		                         16
+3	  IPHONE X		                 10
+4	  MOTO G 5S		                 10
+5	  IPHONE 6S		                        10
+9	  ONEPLUS 10PRO		             12
+6	  SAMSUNG GALAXY S22	               15
+7	  APPLE IPHONE 13 MINI	            20
+8	  ASUS ZENFONE8		                    16
 10	  MOTOROLA EDGE30		                     15
 11	  ASUS ROGPHONE6 		                     8
 ____________________________________________________________
