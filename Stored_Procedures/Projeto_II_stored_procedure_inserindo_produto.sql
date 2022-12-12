162be3a82f3ab25675423fcc3a0448636996f4cd
Claudio Henrique de Oliveira Soares <87618859+ClaudioOliveira89@users.noreply.github.com>
2022-12-11 21:07:47 -0300

    Update Projeto_II_stored_procedure_inserindo_produto.sql

--- Stored_Procedures/Projeto_II_stored_procedure_inserindo_produto.sql
+++ Stored_Procedures/Projeto_II_stored_procedure_inserindo_produto.sql
@@ -56,12 +56,12 @@ ID            Descrição                            Quantidade
 3	  IPHONE X		               10
 4	  MOTO G 5S		               10
 5	  IPHONE 6S		               10
-9	  ONEPLUS 10PRO		     12
+9	  ONEPLUS 10PRO		   12
 6	  SAMSUNG GALAXY S22	               15
 7	  APPLE IPHONE 13 MINI	               20
-8	  ASUS ZENFONE8		             16
-10	  MOTOROLA EDGE30		         15
-11	  ASUS ROGPHONE6 		          8
+8	  ASUS ZENFONE8		          16
+10	  MOTOROLA EDGE30		     15
+11	  ASUS ROGPHONE6 		     8
 ____________________________________________________________
 Delete from produto where produto.idproduto =11; 
 
